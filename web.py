514ce89c197105d3ad191b251bb1f9bfafea60ef
GoobisMoobis <141375174+GoobisMoobis@users.noreply.github.com>
2025-06-08 02:16:56 -0400

    Update web.py

--- web.py
+++ web.py
@@ -1,6 +1,17 @@
 from fastapi import FastAPI
+from fastapi.responses import JSONResponse
+
 app = FastAPI()
 
 @app.get("/")
 def read_root():
     return {"status": "online"}
+
+@app.get("/badge")
+def badge():
+    return JSONResponse({
+        "schemaVersion": 1,
+        "label": "Status",
+        "message": "online",
+        "color": "green"
+    })
